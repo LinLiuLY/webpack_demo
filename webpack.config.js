7c2bf9c3cc1e67a3e7312bf2af66ed68d8026155
Lin Liu <liulin1987315@gmail.com>
2016-03-20 11:31:36 +0800

    splitting Up the configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,37 @@
 const path = require('path');
 
+// const PATHS = {
+//   app: path.join(__dirname, 'app'),
+//   build: path.join(__dirname, 'build')
+// };
+
+// module.exports = {
+//   // Entry accepts a path or an object of entries. We'll be using the
+//   // latter form given it's convenient with more complex configurations.
+//   entry: {
+//     app: PATHS.app
+//   },
+//   output: {
+//     path: PATHS.build,
+//     filename: 'bundle.js'
+//   }
+// };
+
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
+
 module.exports = {
+
+
+const common = {
+
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
   entry: {
@@ -16,3 +42,13 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
